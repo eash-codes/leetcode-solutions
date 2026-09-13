fbb2c179330d0fb74889b46ebb6902bb5990b93c
Eashan Thapliyal <132943584+eash-codes@users.noreply.github.com>
2026-09-13 18:22:15 +0530

    Sync Container With Most Water (Medium) [Time: O(N), Space: O(1)] - Solution Code

---
+++ Two Pointers/container-with-most-water/solution.py
@@ -0,0 +1,9 @@
+"""
+Problem: Container With Most Water
+Difficulty: Medium
+URL: https://leetcode.com/problems/container-with-most-water/
+Time Complexity: O(N)
+Space Complexity: O(1)
+"""
+
+// Solution code not recorded.
